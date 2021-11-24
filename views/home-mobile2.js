b2ec82d785645aad59614ae30e1a5d827478c818
carlos gizbert <carlosgizbert@gmail.com>
2021-11-23 22:26:40 -0300

    refatorando scripts

---
+++ views/home-mobile2.js
@@ -0,0 +1,15 @@
+feather.replace()
+
+$(document).ready(function () {
+  $('.mobile-cards').slick({
+    speed: 220,
+    useTransform: false,
+    useCSS: false,
+    touchMove: true,
+    rows: 1,
+    mobileFirst: true,
+    infinite: false,
+    arrows: false,
+    slidesToShow: 3
+  })
+})
